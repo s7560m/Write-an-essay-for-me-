bfaba37835af5518f818e0ef5836c77a43ce206f
s7560m <haydenhoffman@gmail.com>
2017-07-05 23:14:00 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,63 @@
+// Essay template
+point = "" // Thesis
+Args = [] // Three arguments
+SubArgs = [] // Sub arguments (passes 9 sub args into the array)
+xfacts = [] // Blake's x-factor explanation
+proof = [] // Self explanatory 
+
+/*
+
+
+Example of a template I've done before down below
+
+
+
+Point = "Theseus and The Minotaur and Alien Vs. Predator are similar"
+Args = [
+    "the protagonists",
+    "the plot",
+    "the conventions used"
+]
+SubArgs = [
+    // The characters
+    "Theseus from the mythology Theseus and The Minotaur",
+    "Alexa from the film Alien Vs. Predator",
+    "the comparison between Alexa and Theseus",
+    // The plot
+    "the actions Theseus performs",
+    "the actions Alexa performs",
+    "the comparison of the actions between the two",
+    // The conventions used
+    "the convention Theseus and the Minotaur uses",
+    "the convention Alien vs. Predator uses",
+    "the comparison of the two conventions"
+]
+proof = [
+    // The characters
+    "After Theseus retrieves the sandals and sword from under the rock, he performs heroic deeds on the way to Athens to prove King Aegeus worthy", 
+    "In the beginning of the movie, Alexa focuses on the safety of her team, where safety, teamwork, and devotion are three main traits that she tries to reenforce", 
+    "Both Theseus and Alexa focus on actions that will improve their community, where Theseus performs actions of heroicness, and Alexa performs actions of leadership.",
+    // The plot
+    "King Minos hosts a yearly tribute where the king of Athens, Aegeus, must deliver seven maidens and seven youths as sacrifices to the Minotaur, but Theseus, after managing to escape the prison, slays the Minotaur to end the misery upon Athens",
+    "A spider-like creature is clearly inside of Sebastian, where he yells to Alexa to kill him for he may cause more harm to others, and Alexa falls through and kills him",
+    "The comparison between the two intentional slains is because of a need for heroism",
+    // The conventions used
+    "In the end of the mythology, Theseus slains the Minotaur and acts like a hero, which saves ",
+    "At the end of the film, every character dies except the main character, Alexa, but the world is intact and remains in peace",
+    "The comparison between both media illustrates how both begin with an unfavorable activity, but ends up with a better one"
+]
+xfacts = [
+    // The characters
+    "Theseus's sought for heroism",
+    "Alexa's sought for safety",
+    "both Theseus and Alexa's sought for something in their respective communities, and in this case, it is some sort of improvement",
+    // The plot 
+    "Theseus's desire to make Athens a better place",
+    "Alexa's desire to make peace",
+    "both Theseus and Alexa's desires to make a change",
+    // The conventions
+    "the novel's use of convention on how many stories always end in a positive way",
+    "the film's use of convention on how some stories end up with only the protagonist living, but the world remains in peace",
+    "how stories almost always tend use the convention to have positive endings"
+]
+*/
